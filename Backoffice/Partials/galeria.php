bd2bedb344a5e3d8b19f2d5379f0df441e6ba34d
CardozoFede <fdcardozo178@gmail.com>
2024-10-19 13:56:33 -0300

    Cambios en el Nav

--- Backoffice/Partials/galeria.php
+++ Backoffice/Partials/galeria.php
@@ -4,7 +4,9 @@
                     <div class="col-lg-8">
                     <h2 style="text-align: center;">GALERIA</h2>
 
-                        <p class="lead">Mandanos tus fotos y videos tuyos o de tu equipo</p>
+                        <p class="lead">Mandanos tus fotos y videos tuyos o de tu equipo
+                            <p>lorem ipmput</p>
+                        </p>
                        
                     </div>
                 </div>
