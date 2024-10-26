e733b7a3a316ee24b980f03047ba7fe4104553d3
CardozoFede <fdcardozo178@gmail.com>
2024-10-25 23:01:13 -0300

    Cambios en el Front

--- Backoffice/Partials/footer.php
+++ Backoffice/Partials/footer.php
@@ -1,5 +1,108 @@
-<footer class="footer mt-auto py-3 bg-light">
-  <div class="container">
-    <span class="text-muted">Lorem, ipsum dolor.</span>
+<footer class="bg-dark text-white pt-5 pb-4">
+  <div class="container text-center text-md-left">
+    <div class="row text-center text-md-left">
+      <!-- Logo y descripción -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Liga Argentina</h5>
+        <p>
+          Somos una organización dedicada a fomentar el fútbol infantil en Argentina. Con múltiples torneos a lo largo
+          del año, nuestro objetivo es desarrollar el talento joven y la pasión por el deporte.
+        </p>
+      </div>
+
+      <!-- Enlaces útiles -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+      <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Enlaces</h5>
+
+
+        <p>
+          <a href="#torneos" class="text-white" style="text-decoration: none;">Torneos</a>
+        </p>
+        <p>
+          <a href="#equipos" class="text-white" style="text-decoration: none;">Equipos</a>
+        </p>
+        <p>
+          <a href="#resultados" class="text-white" style="text-decoration: none;">Resultados</a>
+        </p>
+        <p>
+          <a href="#contacto" class="text-white" style="text-decoration: none;">Contacto</a>
+        </p>
+      </div>
+
+      <!-- Redes sociales -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Síguenos</h5>
+        <p>
+          <a href="#" class="text-white" style="text-decoration: none;"><i class="fab fa-facebook-f mr-2"></i>
+            Facebook</a>
+        </p>
+        <p>
+          <a href="#" class="text-white" style="text-decoration: none;"><i class="fab fa-twitter mr-2"></i> Twitter</a>
+        </p>
+        <p>
+          <a href="#" class="text-white" style="text-decoration: none;"><i class="fab fa-instagram mr-2"></i>
+            Instagram</a>
+        </p>
+        <p>
+          <a href="#" class="text-white" style="text-decoration: none;"><i class="fab fa-youtube mr-2"></i> YouTube</a>
+        </p>
+      </div>
+
+      <!-- Información de contacto -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-primary">Contacto</h5>
+        <p><i class="fas fa-home mr-3"></i> Buenos Aires, Argentina</p>
+        <p><i class="fas fa-envelope mr-3"></i> info@labf.com</p>
+        <p><i class="fas fa-phone mr-3"></i> +54 11 5555-5555</p>
+        <p><i class="fas fa-print mr-3"></i> +54 11 5555-5556</p>
+      </div>
+    </div>
+
+    <hr class="mb-4">
+
+    <div class="row mt-4">
+      <div class="col-md-12">
+        <ul class="list-inline">
+          <li class="list-inline-item" style="margin-right: 40px;">
+            <a href="#" style="color: #fff; text-decoration: none;">Política de Privacidad</a>
+          </li>
+          <li class="list-inline-item" style="margin-right: 40px;">
+            <a href="#" style="color: #fff; text-decoration: none;">Términos y Condiciones</a>
+          </li>
+          <li class="list-inline-item" style="margin-right: 40px;">
+            <a href="#" style="color: #fff; text-decoration: none;">Politicas de Cookies</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="#" style="color: #fff; text-decoration: none;">Ajustes de Cookies</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- Copyright -->
+    <div class="row align-items-center justify-content-center">
+      <center>
+      <div class="col-md-7 col-lg-8 text-center">
+        <p>© 2024 Liga Argentina. Todos los derechos reservados.</p>
+      </div>
+      </center>
+      <div class="col-md-5 col-lg-4 text-center">
+        <div>
+          <a href="#" class="text-white mx-2">
+            <i class="fab fa-cc-visa"></i>
+          </a>
+          <a href="#" class="text-white mx-2">
+            <i class="fab fa-cc-mastercard"></i>
+          </a>
+          <a href="#" class="text-white mx-2">
+            <i class="fab fa-cc-paypal"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+
+
   </div>
 </footer>
+
+<!-- Bootstrap JS and FontAwesome Icons -->
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
